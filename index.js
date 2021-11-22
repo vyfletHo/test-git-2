871d38f72d679eba660cbd2f68b7c3936aee7a24
Mikhail Filimonov <mixail.filimonov@bk.ru>
2021-11-22 21:43:07 +0400

    made changes in master

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 console.log('Hello world')
 
+const userAge = 35
+if (userAge >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы НЕ совершеннолетний')
+}
 // 1 способ Скачивание репозитория на компьютер
 
 // git clone(и ссылку на репозиторий)
